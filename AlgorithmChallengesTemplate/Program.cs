905286f4fd42804d3cbd2e6f528ebf39113219d1
MatteoZanella <matteo.zanella-2@studenti.unitn.it>
2019-03-28 12:52:45 +0100

    Complete example

--- AlgorithmChallengesTemplate/Program.cs
+++ AlgorithmChallengesTemplate/Program.cs
@@ -1,9 +1,41 @@
 ﻿using System;
+using System.Threading;
+using System.IO;
 
 namespace AlgorithmChallengesTemplate {
-   class Program {
-      static void Main(string[] args) {
-         Console.WriteLine("Hello World!");
+   struct Example {
+      int Points;
+      public Example(int points) {
+         Points = points;
+      }
+   }
+
+   internal static class Program {
+      private static void Main() {
+         const string inputNames = "a b c";
+         foreach (var inputName in inputNames.Split(" ")) {
+            var newThread = new Thread(() => Run(inputName));
+            newThread.Start();
+         }
+      }
+
+      private static void Run(string inputName) {
+         //INPUT
+         var streamReader = new StreamReader($"./in/{inputName}.txt");
+         var firstLine = streamReader.ReadLine();
+         streamReader.Close();
+        
+         //CODE
+         /*
+          * Bla bla bla bla
+          */
+         
+         //OUTPUT
+         using (var streamWriter = new StreamWriter($"./out/{inputName}.txt")) {
+            streamWriter.WriteLine(firstLine + " elaborato con successo");
+         }
+
+         Console.WriteLine("Termine esecuzione per input " + inputName);
       }
    }
 }
